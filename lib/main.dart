53d382380f76b6d8feb25d1860a382863454c533
Rokibul <therokibul@gmail.com>
2021-08-27 16:01:33 +0600

    Layout completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,149 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Calculator());
 }
 
-class MyApp extends StatelessWidget {
-  .
+class Calculator extends StatelessWidget {
+  const Calculator({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Calculator',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SimpleCalculator(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-  final String title;
+class SimpleCalculator extends StatefulWidget {
+  const SimpleCalculator({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _SimpleCalculatorState createState() => _SimpleCalculatorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
+class _SimpleCalculatorState extends State<SimpleCalculator> {
+  Widget buildButton(
+      String buttonText, double buttonHeight, Color buttonColor) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
+      color: buttonColor,
+      child: FlatButton(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0.0),
+              side: BorderSide(
+                  color: Colors.white, width: 1, style: BorderStyle.solid)),
+          padding: EdgeInsets.all(16.0),
+          onPressed: null,
+          child: Text(
+            buttonText,
+            style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.white),
+          )),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Calculator'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Column(children: [
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: Text(
+              '0',
+              style: TextStyle(fontSize: 38.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: Text(
+              '0',
+              style: TextStyle(fontSize: 48.0),
+            ),
+          ),
+          Expanded(child: Divider()),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Container(
+              width: MediaQuery.of(context).size.width * .75,
+              child: Table(
+                children: [
+                  TableRow(
+                    children: [
+                      buildButton('c', 1, Colors.redAccent),
+                      buildButton('⌫', 1, Colors.blueAccent),
+                      buildButton('÷', 1, Colors.redAccent),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      buildButton('7', 1, Colors.black54),
+                      buildButton('8', 1, Colors.black54),
+                      buildButton('9', 1, Colors.black54),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      buildButton('4', 1, Colors.black54),
+                      buildButton('5', 1, Colors.black54),
+                      buildButton('6', 1, Colors.black54),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      buildButton('1', 1, Colors.black54),
+                      buildButton('2', 1, Colors.black54),
+                      buildButton('3', 1, Colors.black54),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      buildButton('.', 1, Colors.black54),
+                      buildButton('0', 1, Colors.black54),
+                      buildButton('00', 1, Colors.black54),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width *0.25 ,
+              child: Table(
+                children: [
+                  TableRow(
+                    children: [
+                      buildButton('×', 1, Colors.blue),
+                    ]
+                  ),
+                  TableRow(
+                      children: [
+                        buildButton('-', 1, Colors.blue),
+                      ]
+                  ),
+                  TableRow(
+                      children: [
+                        buildButton('+', 1, Colors.blue),
+                      ]
+                  ),
+                  TableRow(
+                      children: [
+                        buildButton('=', 2, Colors.redAccent),
+                      ]
+                  ),
+                ],
+              ),
+            )
+          ]
+          ),
+        ]),
     );
   }
 }
